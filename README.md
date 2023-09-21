1507669549d3a1fa94d6b3b16b20ada6614a11ff
Guokan SHANG <guokan.shang@hotmail.com>
2023-09-21 17:32:19 +0200

    add data preparation

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # CLAIRE
+
+[Anaconda](https://repo.anaconda.com/archive/Anaconda3-2023.03-1-Linux-x86_64.sh)
+
+
+# create environment
+conda create -y -n claire python=3.8
+conda activate claire
+pip install --user --no-cache-dir --index-url https://download.pytorch.org/whl/nightly/cu118 --pre 'torch>=2.1.0dev'
+pip install --user --no-cache-dir -r requirements.txt
+
+# download Falcon-7b
+python lit_gpt/scripts/download.py --repo_id tiiuae/falcon-7b
+python lit_gpt/scripts/convert_hf_checkpoint.py --checkpoint_dir checkpoints/tiiuae/falcon-7b
+
+
---
+++ prepare_data.py
@@ -0,0 +1,100 @@
+import re
+import csv
+# import sys
+import random
+from pathlib import Path
+
+import numpy as np
+from tqdm import tqdm
+
+from datasets import load_dataset
+
+# support running without installing as a package
+# wd = Path(__file__).parent.parent.resolve()
+# sys.path.append(str(wd))
+
+import lit_gpt.lit_gpt.packed_dataset as packed_dataset
+from lit_gpt.lit_gpt import Config, Tokenizer
+
+datasets_csv = csv.DictReader(open("datasets.csv"))
+
+def augment_fn(sample):
+    # speaker anonymization and randomization
+    speakers = set(re.findall(r"\[.+:\]", sample["text"]))
+    anonymized_randomized_speakers = ["[speaker" + f"{i+1:03}" + ":]" for i in range(len(speakers))]
+    random.shuffle(anonymized_randomized_speakers)
+
+    for s, a_r_s in zip(speakers, anonymized_randomized_speakers):
+        sample["text"] = sample["text"].replace(s, a_r_s)
+    
+    return sample
+
+
+def prepare_fn(
+    source_path: Path, checkpoint_dir: Path, destination_path: Path, chunk_size: int
+) -> None:
+    """Prepare the dataset using the tokenizer."""
+    destination_path.mkdir(parents=True, exist_ok=True)
+
+    tokenizer = Tokenizer(checkpoint_dir)
+
+    for row in datasets_csv:
+        set_name = row["lang"] + "-" + row["set_name"]
+        file_name = "full.txt"
+
+        filepath = source_path / set_name / file_name
+
+        if not filepath.is_file():
+            raise RuntimeError(
+                f"Input file not found at {filepath}."
+            )
+
+        builder = packed_dataset.PackedDatasetBuilder(
+            outdir=destination_path,
+            prefix=set_name,
+            chunk_size=chunk_size,
+            sep_token=tokenizer.eos_id,
+            dtype="auto",
+            vocab_size=tokenizer.vocab_size,
+        )
+
+        print(f"Processing {set_name}")
+
+        dataset = load_dataset("text", data_files={"train": filepath}, sample_by="paragraph", streaming=True)
+        updated_dataset = dataset.map(augment_fn)
+        for sample in tqdm(updated_dataset["train"]):
+            text = sample["text"]
+            text_ids = tokenizer.encode(text)
+            builder.add_array(np.array(text_ids, dtype=builder.dtype))
+
+            # print text before and after augmentation
+            print("BEFORE")
+            print(next(iter(dataset['train'].take(1)))['text'])
+            print("AFTER")
+            print(text)
+            print(len(text_ids))
+
+        builder.write_reminder()
+
+
+def prepare(
+    source_path: Path = Path("data/source_data_folder"),
+    checkpoint_dir: Path = Path("checkpoints/tiiuae/falcon-7b"),
+    destination_path: Path = Path("data/prepared_data_folder"),
+    sample: bool = True,
+) -> None:
+    """Prepare the "Red Pajama" dataset. We assume tokenizer has been trained."""
+    config = Config.from_json(checkpoint_dir / "lit_config.json")
+
+    prepare_fn(
+        source_path=source_path,
+        checkpoint_dir=checkpoint_dir,
+        destination_path=destination_path,
+        chunk_size=(config.block_size + 1) * 1024,  # block size + 1 for causal, 1024 blocks
+    )
+
+
+if __name__ == "__main__":
+    from jsonargparse import CLI
+
+    CLI(prepare)
