a6171f3708026dd3ec118ef1c4e2253362d571dc
Jeronymous <jeromelouradour@gmail.com>
2023-09-27 10:41:43 +0200

    improve text normalization and augmentation

--- utils/text.py
+++ utils/text.py
@@ -1,18 +1,46 @@
 import re
 
 ##############################
-# Text normalization functions
+# Text normalization and augmentation functions
+
+SPECIALS_TO_KEEP = [
+    "PII",
+    "NOISE",
+    "LAUGHTER",
+]
+
+PATTERN_SPECIAL = re.compile(r"\[([^\]]*)\]")
+PATTERN_SPEAKER = re.compile(r"[^\]]+:")
+PATTERN_SPEAKER_COMPLETE = re.compile(r"\[" + PATTERN_SPEAKER.pattern + r"\]")
+PATTERN_SPEAKER_UNANONYMIZED = re.compile(r"\[(?!speaker\d+:)([^]]+):]")
+PATTERN_PUNCTUATIONS = re.compile(r"[,\.!?…]")
+
+def format_text(text, keep_specials=True):
+    if keep_specials:
+        text = re.sub(PATTERN_SPECIAL, _remove_all_except_specials, text)
+    else:
+        text = re.sub(PATTERN_SPECIAL, _remove_all_except_speakers_and_pii, text)
+    return collapse_whitespaces(text)
+    
+def _remove_all_except_specials(match):
+    content_within_brackets = match.group(1)
+    if re.match(PATTERN_SPEAKER, content_within_brackets) or content_within_brackets in SPECIALS_TO_KEEP:
+        return match.group().lower()
+    else:
+        return ""
+    
+def _remove_all_except_speakers_and_pii(match):
+    content_within_brackets = match.group(1)
+    if re.match(PATTERN_SPEAKER, content_within_brackets) or content_within_brackets == "PII":
+        return match.group().lower()
+    else:
+        return ""
 
 def collapse_whitespaces(text):
     return re.sub(r" +", " ", text).strip()
 
-def remove_special_words(text):
-    # Remove all [*] except the one at the beginning and after linebreaks
-    text = re.sub(r"([^\n])\[[^\]]*\]", r"\1", text)
-    return collapse_whitespaces(text)
-    
 def remove_punctuations(text):
-    text = re.sub(r"[,\.!?…]", "", text)
+    text = re.sub(PATTERN_PUNCTUATIONS, "", text)
     return collapse_whitespaces(text)
 
 def to_lower_case(text):
@@ -21,23 +49,23 @@ def to_lower_case(text):
 def anonymize_speakers(text):
     # Get all speakers
     speakers = [] 
-    [speakers.append(x) for x in re.findall(r"\[([^\]]+):\]", text) if x not in speakers] 
-    new_speakers = [f"speaker{i+1:03d}" for i in range(len(speakers))]
+    [speakers.append(x) for x in re.findall(PATTERN_SPEAKER_COMPLETE, text) if x not in speakers] 
+    new_speakers = [f"[speaker{i+1:03d}:]" for i in range(len(speakers))]
     for spk, nspk in zip(speakers, new_speakers):
-        text = text.replace(f"[{spk}:", f"[{nspk}:")
+        text = text.replace(spk, nspk)
     return text
 
 def has_upper_case(text):
     return bool(re.search(r"[A-Z]", text))
 
 def has_speaker_id(text):
-    return bool(re.search(r"\[[^spkeaker\d]+:\]", text))
+    return bool(re.search(PATTERN_SPEAKER_UNANONYMIZED, text))
 
 def has_punctuation(text):
-    return bool(re.search(r"[,\.!?…]", text))
+    return bool(re.search(PATTERN_PUNCTUATIONS, text))
 
 def augmented_texts_generator(text):
-    text = remove_special_words(text)
+    text = format_text(text)
     yield text
     _upper = has_upper_case(text)
     _speaker = has_speaker_id(text)
@@ -58,3 +86,19 @@ def augmented_texts_generator(text):
                 yield remove_punctuations(to_lower_case(text_anonym))
         if _speaker:
             yield remove_punctuations(text_anonym)
+
+if __name__ == "__main__":
+    
+    import sys
+    text = " ".join(sys.argv[1:])
+
+    INCLUDE_LINE_BREAKS = False
+    if INCLUDE_LINE_BREAKS:
+        text = re.sub(r" ("+PATTERN_SPEAKER_COMPLETE.pattern+r")", r"\n\1", text)
+    def format_stdout(text):
+        return text.replace("\n", "\\n")
+    print("Original      :", format_stdout(text))
+    print("Normalized (2):", format_stdout(format_text(text, keep_specials=False)))
+    for ivariant, text_variant in enumerate(augmented_texts_generator(text)):
+        print(f"Augmented ({ivariant}):" if ivariant > 0 else "Normalized   :", format_stdout(text_variant))
+
