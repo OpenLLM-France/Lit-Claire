1ac624fdf7f46489e47283c0de332dd950b93f0d
Jeronymous <jeromelouradour@gmail.com>
2023-10-02 17:53:56 +0200

    generate multiple of 8 binary files per dataset

--- prepare_data.py
+++ prepare_data.py
@@ -8,6 +8,8 @@ import re
 import os
 import shutil
 import filelock
+import glob
+import math
 
 import numpy as np
 from tqdm import tqdm
@@ -31,7 +33,7 @@ from utils.text import augmented_texts_generator
 
 def prepare_fn(
     source_path: Path, checkpoint_dir: Path, destination_path: Path,
-    chunk_size: int,
+    multiple_of: int = 8,
     effective_block_size: int = None,
     bos=None,
     eos=None,
@@ -80,7 +82,7 @@ def prepare_fn(
         raise RuntimeError(
             f"No input files found at {source_path}."
         )
-
+    
     for filepath, metadata in all_files.items(): # tqdm(all_files.items(), unit="dataset"):
 
         random.seed(51) # For deterministic text augmentation
@@ -96,15 +98,6 @@ def prepare_fn(
 
         dataset_hf = load_dataset("text", data_files={"train": filepath}, sample_by="paragraph", streaming=True)
 
-        builder = packed_dataset.PackedDatasetBuilder(
-            outdir=destination_path,
-            prefix=prefix,
-            chunk_size=chunk_size,
-            sep_token=tokenizer.eos_id,
-            dtype="auto",
-            vocab_size=tokenizer.vocab_size,
-        )
-
         num_cuts = 0
         num_convs = 0
         num_convs_augmented = 0
@@ -112,49 +105,95 @@ def prepare_fn(
         num_segments = 0
         min_len = 1e10
         max_len = 0
-        for sample in tqdm(dataset_hf["train"], total=num_conversations, unit="conversations", desc=prefix):
-            text = sample["text"]
 
-            # Text normalization and augmentation
-            for ivariant, text_variant in enumerate(augmented_texts_generator(text, augmentation_level, force_augmentation=True)):
+        # First get the number of samples, then build files
 
-                # # Uncomment for debugging of text augmentation
-                # if ivariant > 0:
-                #     if ivariant == 1:
-                #         print(text.replace("\n", " ")[:100])
-                #     print(text_variant.replace("\n", " ")[:100])
+        for build_it in False, True:
 
-                text_ids = tokenizer.encode(text_variant, bos=bos, eos=eos)
-                if effective_block_size and len(text_ids) > effective_block_size:
-                    # Cut in several chunks
-                    for i in range(0, len(text_ids), effective_block_size):
-                        a =np.array(text_ids[i:i+effective_block_size], dtype=builder.dtype)
-                        if len(a) <= 10:
-                            # Leave too short tails
-                            continue
+            if build_it:
+
+                # First remove files that can be there
+                for fn in glob.glob(str(destination_path / f"{prefix}*")):
+                    os.remove(fn)
+
+                # Get the right number of chunks
+                num_segments_per_file = math.ceil(num_segments_augmented / multiple_of)
+                it = 1
+                while num_segments_per_file > 512:
+                    it += 1
+                    num_segments_per_file = math.ceil(num_segments_augmented / (multiple_of * it))
+                chunk_size = effective_block_size * num_segments_per_file
+                
+                num_files = math.ceil(num_segments_augmented/num_segments_per_file)
+                num_reminders = num_segments_augmented % num_segments_per_file
+                print(f"Will cut in {num_files} files of {num_segments_per_file} samples each ({num_reminders} reminders)")
+            
+            else:
+
+                # Dummy value
+                chunk_size = effective_block_size * 512
+
+            # Set the builder
+            builder = packed_dataset.PackedDatasetBuilder(
+                outdir=destination_path,
+                prefix=prefix,
+                chunk_size=chunk_size,
+                sep_token=tokenizer.eos_id,
+                dtype="auto",
+                vocab_size=tokenizer.vocab_size,
+            )
+
+            for sample in tqdm(dataset_hf["train"], total=num_conversations, unit="conversations", desc=f"{prefix} ({2 if build_it else 1}/2)"):
+                text = sample["text"]
+
+                # Text normalization and augmentation
+                for ivariant, text_variant in enumerate(augmented_texts_generator(text, augmentation_level, force_augmentation=True)):
+
+                    # # Uncomment for debugging of text augmentation
+                    # if ivariant > 0:
+                    #     if ivariant == 1:
+                    #         print(text.replace("\n", " ")[:100])
+                    #     print(text_variant.replace("\n", " ")[:100])
+
+                    text_ids = tokenizer.encode(text_variant, bos=bos, eos=eos)
+                    if effective_block_size and len(text_ids) > effective_block_size:
+                        # Cut in several chunks
+                        for i in range(0, len(text_ids), effective_block_size):
+                            a =np.array(text_ids[i:i+effective_block_size], dtype=builder.dtype)
+                            if len(a) <= 10:
+                                # Leave too short tails
+                                continue
+                            if padding and len(a) < effective_block_size:
+                                a = np.pad(a, (0, effective_block_size - len(a)), mode="constant", constant_values=tokenizer.eos_id)
+                            min_len = min(min_len, len(a))
+                            max_len = max(max_len, len(a))
+                            if build_it:
+                                builder.add_array(a)
+                            else:
+                                if ivariant == 0:
+                                    num_segments += 1
+                                num_segments_augmented += 1
+                        if not build_it:
+                            num_cuts += 1
+                    else:
+                        a = np.array(text_ids, dtype=builder.dtype)
                         if padding and len(a) < effective_block_size:
                             a = np.pad(a, (0, effective_block_size - len(a)), mode="constant", constant_values=tokenizer.eos_id)
                         min_len = min(min_len, len(a))
                         max_len = max(max_len, len(a))
-                        builder.add_array(a)
-                        if ivariant == 0:
-                            num_segments += 1
-                        num_segments_augmented += 1
-                    num_cuts += 1
-                else:
-                    a = np.array(text_ids, dtype=builder.dtype)
-                    if padding and len(a) < effective_block_size:
-                        a = np.pad(a, (0, effective_block_size - len(a)), mode="constant", constant_values=tokenizer.eos_id)
-                    min_len = min(min_len, len(a))
-                    max_len = max(max_len, len(a))
-                    builder.add_array(a)
-                    if ivariant == 0:
-                        num_segments += 1
-                    num_segments_augmented += 1
-                num_convs_augmented+= 1
-            num_convs += 1
+                        if build_it:
+                            builder.add_array(a)
+                        else:
+                            if ivariant == 0:
+                                num_segments += 1
+                            num_segments_augmented += 1
+                    if not build_it:
+                        num_convs_augmented+= 1
+                if not build_it: 
+                    num_convs += 1
 
-        builder.write_reminder()
+            if build_it:
+                builder.write_reminder()
 
         print(f"* {num_cuts}/{num_convs_augmented} text cutted in several chunks")
         print(f"* min-max length: {min_len} - {max_len}")
@@ -212,7 +251,6 @@ def prepare(
         source_path=source_path,
         checkpoint_dir=checkpoint_dir,
         destination_path=destination_path,
-        chunk_size=effective_block_size * 512,  # block size + 1 for causal, 512 blocks
         effective_block_size=effective_block_size,
         padding=padding,
         update_metadata=update_metadata,
