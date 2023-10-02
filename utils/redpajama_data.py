a17691f8094c69e5a18c79728de75ce07b0965a8
Guokan Shang <guokan.shang@hotmail.com>
2023-10-02 15:06:25 +0200

    fix redpajama dataloader StopIteration error
    when devides=2 and num_nodes=2

--- utils/redpajama_data.py
+++ utils/redpajama_data.py
@@ -32,7 +32,8 @@ def create_dataloader(
         filenames = glob.glob(str(data_dir / f"{prefix}*"))
         dataset = PackedDataset(
             filenames,
-            n_chunks=4,
+            n_chunks=1,
+            wrap=True,
             block_size=block_size,
             shuffle=shuffle,
             seed=seed,
