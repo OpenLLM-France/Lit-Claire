5ac21fe0e516c37cd24eff18198ce0982a21ef68
Jeronymous <jeromelouradour@gmail.com>
2023-10-06 11:58:46 +0200

    add option to avoid cutting in the middle of a speech turn

--- prepare_data.py
+++ prepare_data.py
@@ -10,6 +10,7 @@ import shutil
 import filelock
 import glob
 import math
+import torch
 
 import numpy as np
 from tqdm import tqdm
@@ -17,8 +18,8 @@ from tqdm import tqdm
 from datasets import load_dataset
 
 # support running without installing as a package
-wd = Path(__file__).parent.resolve()
-sys.path = [str(wd / "lit_gpt")] + sys.path # Prepend to PYTHONPATH
+this_folder = Path(__file__).parent.resolve()
+sys.path = [str(this_folder / "lit_gpt")] + sys.path # Prepend to PYTHONPATH
 
 import lit_gpt.packed_dataset as packed_dataset
 from lit_gpt.config import Config
@@ -43,6 +44,8 @@ def prepare_fn(
     filename_dev="dev.txt",
     skip_if_exists=True,
     update_metadata=False,
+    cut_around_turns=True,
+    DEBUG_PRINT=False,
 ) -> None:
     """Prepare the dataset using the tokenizer."""
     destination_path = destination_path.resolve()
@@ -54,7 +57,7 @@ def prepare_fn(
         bos = tokenizer.use_bos
         assert bos == tokenizer.check_if_bos_token_used(checkpoint_dir)
     if eos is None:
-        eos = bool(tokenizer.eos_id)
+        eos = bool(tokenizer.eos_id) and not padding
 
     print(f"Using: {bos=}, {eos=}, {effective_block_size=}")
 
@@ -80,6 +83,21 @@ def prepare_fn(
                     metadata["is_dev"] = (f == filename_dev)
                     all_files[filepath] = metadata
 
+    # Get tokens around tags for turns
+    if cut_around_turns:
+        tag_tokens = [tokenizer.encode(s, bos=False, eos=False).tolist() for s in ("[speaker001:]", "[Intervenant 1:]", "[A:]")]
+        tag_tokens_prefix = common_prefix(tag_tokens)
+        tag_tokens_suffix = common_suffix(tag_tokens)
+        assert len(tag_tokens_prefix) > 0, f"Weird tokenizer. Cannot find common prefix for {tag_tokens}"
+        assert len(tag_tokens_suffix) > 0, f"Weird tokenizer. Cannot find common suffix for {tag_tokens}"
+        for tokens, expected in [(tag_tokens_prefix, "["), (tag_tokens_suffix, ":]")]:
+            actual = tokenizer.decode(torch.tensor(tokens, dtype=torch.int32))
+            assert actual == expected, f"Unexpected tokenizer behaviour. got {actual} instead of {expected} ({tokens=})"
+        if len(tag_tokens_prefix) > 1 or len(tag_tokens_suffix) > 1:
+            raise NotImplementedError("Tokenizer with several tokens for starting and ending tags are not supported")
+        tag_token_prefix = tag_tokens_prefix[0]
+        tag_token_suffix = tag_tokens_suffix[0]
+
     if len(all_files) == 0:
         raise RuntimeError(f"No input files found at {source_path}.")
     
@@ -90,10 +108,12 @@ def prepare_fn(
         is_spontaneous = metadata["spontaneous"]
         assert is_spontaneous in [True, False]
         augmentation_level = 4 if is_spontaneous else 0
+        force_augmentation = True
 
         # Do not augment validation
         if metadata["is_dev"]:
             augmentation_level = 0
+            force_augmentation = False
 
         prefix = set_name.replace("/", "--")
 
@@ -174,7 +194,7 @@ def prepare_fn(
                 text = sample["text"]
 
                 # Text normalization and augmentation
-                for ivariant, text_variant in enumerate(augmented_texts_generator(text, augmentation_level, force_augmentation=True)):
+                for ivariant, text_variant in enumerate(augmented_texts_generator(text, augmentation_level, force_augmentation=force_augmentation)):
 
                     # # Uncomment for debugging of text augmentation
                     # if ivariant > 0:
@@ -183,13 +203,23 @@ def prepare_fn(
                     #     print(text_variant.replace("\n", " ")[:100])
 
                     text_ids = tokenizer.encode(text_variant, bos=bos, eos=eos)
+                    no_prefix = torch.tensor([], dtype=torch.int32)
+                    add_prefix = no_prefix
                     if effective_block_size and len(text_ids) > effective_block_size:
                         # Cut in several chunks
-                        for i in range(0, len(text_ids), effective_block_size):
-                            a =np.array(text_ids[i:i+effective_block_size], dtype=builder.dtype)
-                            if len(a) <= 10:
+                        istart = 0
+                        while istart < len(text_ids):
+                            iend = istart + effective_block_size - len(add_prefix)
+                            assert iend > istart
+                            selec = text_ids[istart:iend]
+                            if len(add_prefix):
+                                selec = torch.cat([add_prefix, selec])
+                                if DEBUG_PRINT: print("=== with prefix ===\n", tokenizer.decode(selec))
+                            elif DEBUG_PRINT: print("=== standard ===\n", tokenizer.decode(selec))
+                            a =np.array(selec, dtype=builder.dtype)
+                            if not cut_around_turns and len(a) <= 10:
                                 # Leave too short tails
-                                continue
+                                break
                             if padding and len(a) < effective_block_size:
                                 a = np.pad(a, (0, effective_block_size - len(a)), mode="constant", constant_values=tokenizer.eos_id)
                             min_len = min(min_len, len(a))
@@ -199,10 +229,53 @@ def prepare_fn(
                             if ivariant == 0:
                                 num_segments += 1
                             num_segments_augmented += 1
+
+                            previous_istart = istart
+                            
+                            if not cut_around_turns:
+                                # Naive
+                                istart += effective_block_size
+                                continue
+
+                            # Start with the last turn
+                            selec = text_ids[istart:iend+10]
+                            candidates = torch.where(selec == tag_token_suffix)[0]
+                            if len(candidates) > 0:
+                                # Cut around the last end of turn tag
+                                end_of_turn = candidates[-1].item()
+                                candidates = torch.where(selec[:end_of_turn] == tag_token_prefix)[0]
+                                assert len(candidates)
+                                start_of_turn = candidates[-1].item()
+                                if start_of_turn > 0:
+                                    if DEBUG_PRINT: print("=== Case 1.1 - ", end='')
+                                    # Shift to the last turn
+                                    istart += start_of_turn
+                                    add_prefix = no_prefix
+                                    assert text_ids[istart] == tag_token_prefix
+                                else:
+                                    if DEBUG_PRINT: print("=== Case 1.2 - ", end='')
+                                    # We stay in the same big turn, or it's the last turn
+                                    istart += effective_block_size
+                                    add_prefix = selec[:end_of_turn+1]
+                                    if istart < len(text_ids):
+                                        # Avoid to cut in the middle of a word
+                                        while not tokenizer.decode(torch.tensor(text_ids[istart], dtype=torch.int32)).startswith(" "):
+                                            istart -= 1
+                            else:
+                                if DEBUG_PRINT: print("=== Case 2 - ", end='')
+                                # We are in the middle of a big conversation with the same speaker,
+                                # or end of conversation
+                                istart += effective_block_size
+                                if istart < len(text_ids):
+                                    # Avoid to cut in the middle of a word
+                                    while not tokenizer.decode(torch.tensor(text_ids[istart], dtype=torch.int32)).startswith(" "):
+                                        istart -= 1
+                            assert istart > previous_istart
+
                         num_cuts += 1
                     else:
                         a = np.array(text_ids, dtype=builder.dtype)
-                        if padding and len(a) < effective_block_size:
+                        if effective_block_size and padding and len(a) < effective_block_size:
                             a = np.pad(a, (0, effective_block_size - len(a)), mode="constant", constant_values=tokenizer.eos_id)
                         min_len = min(min_len, len(a))
                         max_len = max(max_len, len(a))
@@ -246,6 +319,17 @@ def prepare_fn(
                     writer.writeheader()
                     writer.writerows(sorted(metadata, key=lambda x: x["dataset"]))
 
+def common_prefix(lists):
+    i = 0
+    min_length = min([len(l) for l in lists])
+    while i < min_length and all([l[i] == lists[0][i] for l in lists[1:]]):
+        i += 1
+    return lists[0][:i]
+
+def common_suffix(lists):
+    return common_prefix([l[::-1] for l in lists])[::-1]
+
+
 def prepare(
     source_path: Path = Path("data/source_data_folder"),
     checkpoint_dir: Path = Path("checkpoints/tiiuae/falcon-7b"),
@@ -260,6 +344,14 @@ def prepare(
 
     destination_path.mkdir(parents=True, exist_ok=True)
     shutil.copy2(config_file, destination_path / "lit_config.json")
+
+    # Copy code used to produce the data
+    (destination_path / "src").mkdir(parents=True, exist_ok=True)
+    for file in __file__, "data/claire_metadata.csv":
+        shutil.copy2(this_folder / file, destination_path / "src" / os.path.basename(file))
+    for folder in "utils", "lit_gpt/lit_gpt", :
+        shutil.copytree(this_folder / folder, destination_path / "src" / folder,
+            ignore=lambda x, y: ["__pycache__"], dirs_exist_ok=True)
 
     effective_block_size = config.block_size + 1
     tokenizer_config_file = checkpoint_dir / "tokenizer_config.json"
