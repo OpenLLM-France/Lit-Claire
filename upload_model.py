a55dc7405cd2010a84230bb8e3acbb70157f0593
Guokan SHANG <guokan.shang@hotmail.com>
2023-10-08 00:50:25 +0200

    split large model to smaller shards

--- upload_model.py
+++ upload_model.py
@@ -1,15 +1,9 @@
-from huggingface_hub import login, HfApi, hf_hub_download
+from huggingface_hub import login, HfApi
 login()
 from pathlib import Path
 
 
 def upload(folder_path: Path="folder_path", repo_id: str="repo_id", create_repo: bool=False):
-    # download missing files which was not downloaded by lit_gpt/scripts/download.py
-    revision="898df1396f35e447d5fe44e0a3ccaaaa69f30d36"
-    filenames = ["README.md", "config.json", "special_tokens_map.json"]
-    for filename in filenames:
-        hf_hub_download(repo_id="tiiuae/falcon-7b", filename=filename, revision=revision, local_dir=folder_path)
-
     api = HfApi()
 
     if create_repo is True:
@@ -24,7 +18,7 @@ def upload(folder_path: Path="folder_path", repo_id: str="repo_id", create_repo:
         folder_path=folder_path,
         repo_id=repo_id,
         repo_type="model",
-        ignore_patterns=["lit_*", "pytorch_model.bin.index.json"],
+        ignore_patterns=["lit_*", "pytorch_model.bin"],
     )
 
 
