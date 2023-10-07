9b662cd2a6fa8ab4967bda4def182fdde9ef96cc
Guokan SHANG <guokan.shang@hotmail.com>
2023-10-08 01:20:57 +0200

    fix OOM

--- split_model.py
+++ split_model.py
@@ -1,10 +1,11 @@
+import torch
 from transformers import AutoModelForCausalLM
 from pathlib import Path
 
 
 def split(folder_path: Path="folder_path"):
     print("loading model ...")
-    model = AutoModelForCausalLM.from_pretrained(folder_path)
+    model = AutoModelForCausalLM.from_pretrained(folder_path, torch_dtype=torch.bfloat16)
     
     print("spliting model ...")
     model.save_pretrained(folder_path)
